f5fb8c352da4efeaf91d60aa1ed636110f290a8d
Pavlos Isaris <paulisaris@gmail.com>
2016-09-23 11:29:28 +0300

    Updated repetitive tasks with comments and added appropriate command to clear the properties file

--- repetitive_tasks.sh
+++ repetitive_tasks.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # This script executes some repetitive tasks required to compile this application
 
+#The following command clears the config.properties file (To clear any saved highscores)
+echo -n "" > file.log
+
+#Maven related tasks
+
 mvn clean
 
 mvn package
