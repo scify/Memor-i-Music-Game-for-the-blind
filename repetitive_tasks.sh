1210753161da5c0bd649dc2a8975948695953833
Pavlos Isaris <paulisaris@gmail.com>
2016-09-27 11:00:01 +0300

    Renamed high scores file

--- repetitive_tasks.sh
+++ repetitive_tasks.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This script executes some repetitive tasks required to compile this application
 
-#The following command clears the config.properties file (To clear any saved highscores)
-echo -n "" > src/main/resources/config.properties
+#The following command clears the config.properties file (To clear any saved high scores)
+echo -n "" > src/main/resources/high_scores.properties
 
 #Maven related tasks
 
