0a39351666745b639b7b238ccd8fa68158775cb6
Pavlos Isaris <paulisaris@gmail.com>
2016-10-14 16:04:24 +0300

    Updated the build project tasks

--- build_project.sh
+++ build_project.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # This script executes some repetitive tasks required to compile this application
 
-#The following command clears the config.properties file (To clear any saved high scores)
-echo -n "" > src/main/resources/high_scores.properties
-
 #Maven related tasks
 
 mvn clean
