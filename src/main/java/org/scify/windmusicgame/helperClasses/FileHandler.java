f416c51775c9fd0be18334088d8c04f362f66a5f
Pavlos Isaris <paulisaris@gmail.com>
2016-12-19 12:52:52 +0200

    Genral refactoring on getting default and current app language

--- src/main/java/org/scify/windmusicgame/helperClasses/FileHandler.java
+++ src/main/java/org/scify/windmusicgame/helperClasses/FileHandler.java
@@ -248,14 +248,30 @@ public class FileHandler {
     }
 
     /**
+     * Get a variable from project.properties file (given an input stream)
+     * @param propertyName the name of the property
+     * @return the value of the given property
+     */
+    public String getPropertyByName(InputStream propertyFileStream, String propertyName) {
+        Properties props = new Properties();
+        try {
+            props.load(propertyFileStream);
+            return props.getProperty(String.valueOf(propertyName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
      * Given a property key, gets a value from resources/project.properties file
      * @param propertyKey the property key
      * @return the property value
      */
     public String getProjectProperty(String propertyKey) {
-        ClassLoader classLoader = getClass().getClassLoader();
-        classLoader.getResource("project.properties").getPath();
-        return this.getPropertyByName(classLoader.getResource("project.properties").getPath(), propertyKey);
+        InputStream inputStream = getClass().getResourceAsStream("/project.properties");
+        String propertyValue = this.getPropertyByName(inputStream, propertyKey);
+        return propertyValue;
     }
 
     /**
